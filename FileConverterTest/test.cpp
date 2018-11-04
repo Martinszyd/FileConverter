bce1c87c22ad57938918fa8b55b6f833ab47a714
Martin <martinszyd@poczta.fm>
2018-11-04 22:17:03 +0100

    Dodatek: diagram klas

--- FileConverterTest/test.cpp
+++ FileConverterTest/test.cpp
@@ -1,20 +1,21 @@
 
-#include "../FileConverter/Konwerter.h";
-#include <string>;
+#include "../FileConverter/Konwerter.h"
+#include "../FileConverter/Console.h"
+#include <string>
 #include <iostream>
 #include "gtest/gtest.h"
 
 using namespace std;
 
-	TEST(Konwerter, plikTXT) {
-
-		EXPECT_EQ(1,1);
+	TEST(ConsoleTest, plikTXT) {
+		const string nazwaPliku = "test.txt";
+		EXPECT_TRUE(Konwerter::Konwerter(nazwaPliku));
 	}
 
 
-	/*
-		int main(int ac, char* av[])
-	{
-		testing::InitGoogleTest(&ac, av);
+	
+		int main(int argc, char **argv) 
+		{
+		::testing::InitGoogleTest(&argc, argv);
 		return RUN_ALL_TESTS();
-	}*/
+	    }
